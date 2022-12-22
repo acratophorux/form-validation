8d935c23118b88f427ebd866b6197ba14be8689e
Deon Kumar <dineshbishnoi7478@gmail.com>
2022-12-23 00:38:44 +0530

    Solved server side rendering issue where no data available for pre-rendering

--- pages/success.js
+++ pages/success.js
@@ -1,5 +1,6 @@
 import { useRouter } from "next/router";
 import { motion as m } from "framer-motion";
+import NoSSRWrapper from "./withNoSSR";
 export default function Success() {
   const router = useRouter();
   console.log(router.query);
@@ -14,19 +15,27 @@ export default function Success() {
             transition={{ duration: 0.2 }}
             className="h-fit w-5/6 rounded-2xl overflow-hidden bg-white sm:w-1/2 shadow-2xl shadow-gray-300 backdrop-blur-3xl"
           >
-            <div className="flex-1 p-6">
-              <h1 className="text-3xl font-latoBold text-gray-700 pb-4">
-                Thanks for the subscribing, {router.query.name.split(" ")[0]}{" "}
-                ✨.
-              </h1>
-              <p className="text-gray-500 font-latoRegular">
-                We have sent you an email at {router.query.email}. Check out the
-                onboarding goodies we just sent you.
-              </p>
-            </div>
+            <NoSSRWrapper>
+              <div className="flex-1 p-6">
+                <h1 className="text-3xl font-latoBold text-gray-700 pb-4">
+                  Thanks for the subscribing, {router.query.name.split(" ")[0]}{" "}
+                  ✨.
+                </h1>
+                <p className="text-gray-500 font-latoRegular">
+                  We have sent you an email at {router.query.email}. Check out
+                  the onboarding goodies we just sent you.
+                </p>
+              </div>
+            </NoSSRWrapper>
           </m.div>
         </div>
       </div>
     </>
   );
 }
+
+// export async function getStaticProps(context) {
+//   return {
+//     props: {},
+//   };
+// }
